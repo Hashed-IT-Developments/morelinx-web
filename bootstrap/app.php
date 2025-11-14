0ce8e0f432b4b48577a02aaaed2da6597413af9b
esyot <rei.nhard3367@gmail.com>
2025-11-14 09:03:51 +0800

    fix(partial-comment): commented middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,7 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
 
         $middleware->web(append: [
-            // HandleAppearance::class,
+            HandleAppearance::class,
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
         ]);
