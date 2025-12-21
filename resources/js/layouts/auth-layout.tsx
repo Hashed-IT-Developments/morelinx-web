3edc2a5dbff76d92272ba54c2e02df8120d46506
Reinhard Ugay Esteban <esyot@Reinhards-MacBook-Pro.local>
2025-12-21 11:18:57 +0800

    feat(snow-fall): added snowfall when december

--- resources/js/layouts/auth-layout.tsx
+++ resources/js/layouts/auth-layout.tsx
@@ -1,9 +1,15 @@
 import AuthLayoutTemplate from '@/layouts/auth/auth-main-layout';
+import Snowfall from 'react-snowfall';
 
 export default function AuthLayout({ children, title, description, ...props }: { children: React.ReactNode; title: string; description: string }) {
+    const isDecember = new Date().getMonth() === 11;
+
     return (
         <AuthLayoutTemplate title={title} description={description} {...props}>
-            {children}
+            <>
+                {isDecember && <Snowfall />}
+                {children}
+            </>
         </AuthLayoutTemplate>
     );
 }
